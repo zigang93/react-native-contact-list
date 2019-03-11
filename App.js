9bc6dec6f72e924faaa16092ed5e8faebd1fbdc0
zigang93 <tanzigang93@gmail.com>
2019-03-11 16:36:51 +0800

    rename folder utils and test contact list

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Contacts from './screens/Contacts';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Contacts />
     );
   }
 }
