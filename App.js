4b5e0461f5edba1d7eb63623310c18c678f2d54c
zigang93 <tanzigang93@gmail.com>
2019-03-11 17:17:40 +0800

    add react navigation pkg and try profile screens

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
 export default class App extends React.Component {
   render() {
     return (
-      <Contacts />
+      <Profile />
     );
   }
 }
